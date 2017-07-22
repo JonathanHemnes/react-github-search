027fa791475a0ccc3b21f587d30cff4de9f93541
Jonathan Hemnes <jonathan.hemnes@talentkode.com>
2017-07-22 07:16:22 -0500

    needless constructor

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 
 export default class SearchForm extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            term: ''
-        }
-    }
 
     render() {
 
