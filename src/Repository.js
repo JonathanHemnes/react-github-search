a67fc2ef3d71811f6119314555109a7c21f23787
Jonathan Hemnes <jonathan.hemnes@talentkode.com>
2017-07-22 07:48:47 -0500

    better styles

--- src/Repository.js
+++ src/Repository.js
@@ -6,7 +6,15 @@ export default class Repository extends Component {
     render() {
         return (
             <div className="repository">
-                <h2>{this.props.repo.name}</h2>
+                <div>
+                    <h2>Repo Name: {this.props.repo.name}</h2>
+                    <h3>Primary Language: {this.props.repo.language || 'Unknown'}</h3>
+                    <h4>Owned By: {this.props.repo.owner.login}</h4>
+                </div>
+                <div>
+                    <h3>Forks: {this.props.repo.forks}</h3>
+                    <h3>Open Issues: {this.props.repo.open_issues}</h3>
+                </div>
                 <img src={this.props.repo.owner.avatar_url} alt="" />
             </div>
         )
